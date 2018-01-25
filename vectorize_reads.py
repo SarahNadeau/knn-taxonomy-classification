60d274087a2401ed9c1d2dda10059e29e8d70b7a
SarahNadeau <san62@cornell.edu>
2018-01-25 17:07:18 -0500

    changes

--- vectorize_reads.py
+++ vectorize_reads.py
@@ -5,6 +5,8 @@ import itertools
 from sklearn import neighbors
 import matplotlib.pyplot as plt
 from sklearn import model_selection
+import pandas as pd
+from sklearn.decomposition import PCA as sklearnPCA
 
 
 class VectorizeMethods:
@@ -15,79 +17,123 @@ class VectorizeMethods:
     @staticmethod
     def make_ngram(seq, n):
         bases = ['A', 'C', 'T', 'G', 'N']
-        ngram = np.zeros(5**n)
+        ngrams = {}
+        for gram in itertools.product(bases, repeat=n):
+            ngrams[''.join(gram)] = 0
         for s in range(0, len(seq) - n + 1):
-            for i, gram in zip(range(0, 5**n), itertools.product(bases, repeat=n)):
-                if seq[s:s + n] == ''.join(gram):
-                    ngram[i] += 1
-        return ngram
+            ngrams[seq[s:s + n]] += 1
+
+        return ngrams
 
 
 class GenerateSeq:
     def __init__(self):
         pass
 
-    # This function takes in a filepath and generates a DNA sequence
+    # This function takes in a filepath and returns a DNA sequence of length k, genome ID
     @staticmethod
     def get_seq(k, filepath, filetype):
         for seq_record in SeqIO.parse(filepath, filetype):
-            # note - only returns sequence from last seq_record if multiple
+            # note - only returns sequence from first seq_record if multiple
             rand_start = np.random.randint(0, len(seq_record.seq) - k + 1)
             return seq_record.seq[rand_start:rand_start + k], seq_record.id
+
+    # This function takes in a HiSeq filepath and returns all fasta DNA sequences, fasta IDs
+    @staticmethod
+    def get_HiSeq(filepath):
+        ids = []
+        seqs = []
+        for seq_record in SeqIO.parse(filepath, 'fasta'):
+            ids.append(seq_record.id)
+            # note - only returns sequence from first seq_record if multiple
+            seq = seq_record.seq
+            seqs.append(seq)
+        return seqs, ids
+
+class GetXSeqsYIDs:
+    def __init__(self):
+        pass
+
+    # This function returns array of vectorized DNA sequences (X), array of sequence IDs (y)
+    # for genbank files in directory
+    @staticmethod
+    def get_seq_arrays(num_test_seqs, len_test_seqs, n):
+        org_num = 0
+        X = []
+        y = []
+        for filename in os.listdir('Genomes'):
+            if filename.endswith('.gbff'):
+                filename = os.path.join('Genomes', filename)
+                for i in range(0, num_test_seqs):
+                    test_seq, test_id = GenerateSeq.get_seq(len_test_seqs, filename, 'genbank')
+                    test_vec = np.array(list(VectorizeMethods.make_ngram(test_seq, n).values())) / (len(test_seq) - n + 1)
+                    X.append(test_vec)
+                    y.append(test_id)
+                org_num += 1
+
+        return X, y
+
+    @staticmethod
+    # This function returns array of vectorized DNA sequences (X), array of sequence IDs (y)
+    # for file HiSeq_accuracy.fa in directory
+    def get_HiSeq_arrays(n):
+        X = []
+        y = []
+        for filename in os.listdir('Genomes'):
+            if filename == 'HiSeq_accuracy.fa':
+                filename = os.path.join('Genomes', filename)
+                Hi_seq_list, Hi_id_list = GenerateSeq.get_HiSeq(filename)
+                for seq in Hi_seq_list:
+                    Hi_vec = np.array(list(VectorizeMethods.make_ngram(seq, n).values())) / (len(seq) - n + 1)
+                    X.append(Hi_vec)
+                for id in Hi_id_list:
+                    Hi_id = id.split('.')[0]
+                    y.append(Hi_id)
+
+        return X, y
+
+
+# This function is used to convert tax labels to ints for plotting legend
+def sum_chars(word):
+    num = 0
+    for char in word:
+        num += ord(char)
+    return num
 
 
 def main():
 
     # set parameters
-    n = 1
-    num_test_seqs = 2
+    n = 10
+    n_neighbors = 1
+    num_test_seqs = 10
     len_test_seqs = 150
+    # test filetype options are 'genbank', 'HiSeq'
+    test_filetype = 'HiSeq'
 
-    # get and vectorize database sequences
-    # db_lib = {}
-    # for filename in os.listdir('Genomes'):
-    #     if filename.endswith('.gbff') and not filename.endswith('test.gbff'):
-    #         filename = os.path.join('Genomes', filename)
-    #         total_len = 0
-    #         org_vec = np.zeros(5**n)
-    #         for accession in SeqIO.parse(filename, 'genbank'):
-    #             seq = accession.seq
-    #             total_len += len(accession.seq)
-    #             acc_vec = VectorizeMethods.make_ngram(seq, n)
-    #             org_vec += acc_vec
-    #         db_lib[filename.split('/')[1].split('.')[0]] = org_vec/(total_len - n + 1)
-    # print(db_lib)
+    # generate arrays (X, y) of vectorized sequences and genome IDs
+    if test_filetype == "genbank":
+        X, y = GetXSeqsYIDs.get_seq_arrays(num_test_seqs, len_test_seqs, n)
+    elif test_filetype == 'HiSeq':
+        X, y = GetXSeqsYIDs.get_HiSeq_arrays(n)
 
-    # db_lib = {'Corynebacterium_glutamicum_ASM1132v1': [0.23096325,  0.2703033 ,  0.23093424,  0.26779922,  0.],
-    #           'Mycobacterium smegmatis_ASM1500v1': [0.16315325,  0.33732191,  0.16281869,  0.33670616,  0.],
-    #           'Streptococcus_pyogenes_ASM678v2': [0.30896556,  0.19074266,  0.3059166 ,  0.19437518,  0.],
-    #           'Thermosynechococcus_elongatus_ASM1134v1': [0.23089399,  0.26937183,  0.22992671,  0.26980747,  0.]}
+    # plot n-gram separation between organisms
+    pca = sklearnPCA(n_components=2)  # 2-dimensional PCA
+    transformed = pca.fit_transform(X)
+    y_colors = list(map(lambda x: sum_chars(x), y))
+    plt.scatter(transformed[:, 0], transformed[:, 1], c=y_colors)
+    plt.title("{}-gram separation between 10 Hi-Seq genomes".format(n))
+    plt.show()
 
-    org_count = 0
-    for filename in os.listdir('Genomes'):
-        if filename.endswith('.gbff'):
-            org_count += 1
-
-    org_num = 0
-    X = []
-    y = []
-    for filename in os.listdir('Genomes'):
-        if filename.endswith('.gbff'):
-            filename = os.path.join('Genomes', filename)
-            for i in range(0, num_test_seqs):
-                test_seq, test_id = GenerateSeq.get_seq(len_test_seqs, filename, 'genbank')
-                test_vec = VectorizeMethods.make_ngram(test_seq, n)/(len(test_seq) - n + 1)
-                X.append(test_vec)
-                y.append(test_id)
-            org_num += 1
-
+    # split X an y into training and test set for KNN
+    # eventually training set should be NCBI reference genomes and testing set should be metagenomic samples
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.33, random_state=42)
 
     # classify sequence by nearest neighbor in database
-    knn = neighbors.KNeighborsClassifier(n_neighbors=1)
+    knn = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors)
     print('KNN score: %f' % knn.fit(X_train, y_train).score(X_test, y_test))
     print('with n = {}'.format(n))
-    print('with num_test_seqs = {}'.format(num_test_seqs))
+    print('with n_neighbors = {}'.format(n_neighbors))
 
 
 if __name__ == '__main__':
